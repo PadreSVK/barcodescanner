a78d5850a78fa98a9066580fdc0f69ba098e3c8a
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-05-04 14:06:25 +0200

    add DAL with EFCore+ SQL server

---
+++ barcode-api/Barcode.DAL/BarCode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Barcode.DAL;
+
+public class BarCode
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; }
+    public string Type { get; set; }
+
+}
---
+++ barcode-api/Barcode.DAL/MyContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Barcode.DAL
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<BarCode> BarCodes { get; set; }
+    }
+}
